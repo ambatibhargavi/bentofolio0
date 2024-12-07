cf6af77d37dc7c7596a796299dd41744efdd2bb1
Bhargavi Ambati <140686868+ambatibhargavi@users.noreply.github.com>
2024-12-07 20:33:04 +0530

    Update ProjectsGrid.js

--- src/app/components/ProjectsGrid.js
+++ src/app/components/ProjectsGrid.js
@@ -23,7 +23,7 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
     return () => {
       clearInterval(id);
     };
-  });
+  }, [nameIdx, subheadingIdx]);
 
   const trails = useTrail(7, {
     from: { scale: 0 },
@@ -46,7 +46,9 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
           className="w-full h-full bg-[#A7C957] border border-black flex items-center justify-center"
         >
           <p className="text-center text-black text-lg">
-            Project 1: I created a Spotify playlist automation project using Terraform, leveraging Infrastructure as Code (IaC) to manage playlist creation. The project involved writing Terraform scripts to interact with the Spotify API for automated playlist management.<br / > It streamlined adding tracks and updating playlists through code-defined resources. This project demonstrates Terraform's versatility in managing API-driven services like Spotify.
+            Project 1: I created a Spotify playlist automation project using Terraform, leveraging Infrastructure as Code (IaC) to manage playlist creation. The project involved writing Terraform scripts to interact with the Spotify API for automated playlist management.
+            <br />
+            It streamlined adding tracks and updating playlists through code-defined resources. This project demonstrates Terraform's versatility in managing API-driven services like Spotify.
           </p>
           <div className="flex justify-center items-center w-full h-full">
             <a
@@ -70,7 +72,9 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
           className="w-full h-full relative bg-[#BC4749] border border-black flex items-center justify-center"
         >
           <p className="text-center text-white text-lg">
-            Project 2: Argo Rollouts is a Kubernetes controller enabling advanced deployment strategies like Canary, Blue-Green, and Progressive Delivery. It enhances traffic control, version promotion, and rollbacks, integrating with tools like Prometheus and Grafana for observability.<br /> Ideal for CI/CD pipelines, it automates feature rollouts, gradual updates, and seamless production deployments. Integration with service meshes or ingress controllers allows granular traffic management for progressive delivery workflows.
+            Project 2: Argo Rollouts is a Kubernetes controller enabling advanced deployment strategies like Canary, Blue-Green, and Progressive Delivery. It enhances traffic control, version promotion, and rollbacks, integrating with tools like Prometheus and Grafana for observability.
+            <br />
+            Ideal for CI/CD pipelines, it automates feature rollouts, gradual updates, and seamless production deployments. Integration with service meshes or ingress controllers allows granular traffic management for progressive delivery workflows.
           </p>
           <div className="flex justify-center items-center w-full h-full">
             <a
@@ -94,7 +98,9 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
           className="w-full h-full relative bg-[#6A994E] border border-neutral-900 flex items-center justify-center"
         >
           <p className="text-center text-white text-lg">
-            Project 3: This project involves creating and deploying a custom Large Language Model (LLM) using Ollama, designed to provide tailored Q&A functionality based on user-provided data. The model is trained on a specific CSV file, enabling it to deliver context-aware and highly relevant responses to user queries.<br /> It features an interactive Q&A interface, making it easy for users to extract insights from their data seamlessly. The project employs Docker for consistent and portable deployments, ensuring the application runs reliably across different environments.
+            Project 3: This project involves creating and deploying a custom Large Language Model (LLM) using Ollama, designed to provide tailored Q&A functionality based on user-provided data. The model is trained on a specific CSV file, enabling it to deliver context-aware and highly relevant responses to user queries.
+            <br />
+            It features an interactive Q&A interface, making it easy for users to extract insights from their data seamlessly. The project employs Docker for consistent and portable deployments, ensuring the application runs reliably across different environments.
           </p>
           <div className="flex justify-center items-center w-full h-full">
             <a
@@ -142,9 +148,9 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
           className="w-full h-full relative bg-[#6A994E] border border-neutral-900 flex items-center justify-center"
         >
           <p className="text-center text-white text-lg">
-            Project 4: This Terraform project automates Snowflake user creation using loops (for_each), reducing code duplication and improving efficiency. Users are customized with attributes like passwords, default warehouses, roles, and namespaces.It provides a scalable, maintainable solution for managing multiple users.
+            Project 4: This Terraform project automates Snowflake user creation using loops (for_each), reducing code duplication and improving efficiency. Users are customized with attributes like passwords, default warehouses, roles, and namespaces. It provides a scalable, maintainable solution for managing multiple users.
           </p>
-           <div className="flex justify-center items-center w-full h-full">
+          <div className="flex justify-center items-center w-full h-full">
             <a
               href="https://github.com/ambatibhargavi/terraformloops"
               target="_blank"
@@ -166,44 +172,13 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
           className="w-full h-full relative bg-[#BC4749] border border-neutral-900 flex items-center justify-center"
         >
           <p className="text-center text-white text-lg">
-            Project 5: This project is an interactive chatbot using the Llama 2 language model, integrated with Streamlit for the web interface and hosted via the Replicate API. Users can interact with the chatbot, select models, and customize response parameters. Llama 2 was chosen for its flexibility, allowing fine-tuning and customization. It offers greater control compared to proprietary models like ChatGPT, making it ideal for exploring open-source LLMs.
+            Project 5: This project is an interactive chatbot using the Llama 2 language model, integrated with Streamlit for the web interface and hosted via the Replicate API. Users can interact with the chatbot, select models, and customize response parameters. Llama 2 was chosen for its flexibility, allowing fine-tuning and customization.
+            <br />
+            It offers greater control compared to proprietary models like ChatGPT, making it ideal for exploring open-source LLMs.
           </p>
           <div className="flex justify-center items-center w-full h-full">
             <a
               href="https://github.com/ambatibhargavi/chatbot"
               target="_blank"
               rel="noopener noreferrer"
-              className="px-6 py-2 bg-yellow-500 text-black rounded-md shadow-md hover:bg-yellow-600 hover:shadow-lg transition-all"
-            >
-              View Project
-            </a>
-          </div>
-        </animated.div>
-      </animated.div>
-
-      <animated.div
-        style={animatedStyles}
-        className="lg:row-span-3 lg:col-span-2"
-      >
-        <animated.div
-          style={trails[4]}
-          className="w-full h-full lg:flex justify-between items-center p-10 gap-5 bg-[#A7C957] border border-black"
-        >
-          <p className="text-center text-black text-lg">
-            Project 6: This Netflix Clone is built and containerized using microservices and SonarQube.  
-          </p>
-           <div className="flex justify-center items-center w-full h-full">
-            <a
-              href="https://github.com/ambatibhargavi/Clone-Services"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="px-6 py-2 bg-pink-500 text-white rounded-md shadow-md hover:bg-pink-600 hover:shadow-lg transition-all"
-            >
-              View Project
-            </a>
-          </div>
-        </animated.div>
-      </animated.div>
-    </animated.div>
-  );
-}
+              className="px-6 py-2 bg-yellow-500 text-black rounded-md shadow-md hover:bg
